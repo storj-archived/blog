ea4cf532e95176d730ac395502958b7ec2f47793
Malcolm Bouzi <mrbouzi@gmail.com>
2021-01-19 16:59:34 -0500

    refactor and trim email submission

--- static/js/newsletter-subscribe.js
+++ static/js/newsletter-subscribe.js
@@ -1,37 +1,41 @@
 window.onload = function() {
+    var button = document.getElementById("mc-embedded-subscribe-footer");
 
-    // footer newsletter signup
-    var newsletterButton = document.getElementById("mc-embedded-subscribe-newsletter")
-
-    newsletterButton.onclick = function (event) {
-
-      event.preventDefault()
-
-      var inputValue = document.getElementById("mce-EMAIL").value
-      var errElm = document.getElementById("mce-error-response-newsletter");
-      var succElm = document.getElementById("mce-success-response-newsletter");
-      if (!inputValue || inputValue.length < 0 || !inputValue.includes("@")) {
-        if(succElm) {
-          succElm.classList.add("d-none");
-          succElm.classList.remove("d-block");
-        }
-        if(errElm) {
-          errElm.classList.add("d-block");
-          errElm.classList.remove("d-none");
-        }
+    if (!button) {
         return;
-      } else {
-        analytics.identify(analytics.user().anonymousId(), {email: inputValue, storj_newsletter: true}, {}, function() {
-          analytics.track("storj_newsletter", {});
-          if(errElm) {
-            errElm.classList.add("d-none");
-            errElm.classList.remove("d-block");
-          }
-          if(succElm) {
-            succElm.classList.add("d-block");
-            succElm.classList.remove("d-none");
-          }
-        });
-      }
+    }
+
+    button.onclick = function (event) {
+        event.preventDefault();
+
+        var inputValue = document.getElementById("mce-EMAIL-newsletter").value.toLowerCase().trim();
+        var errElm = document.getElementById("mce-error-response-newsletter");
+        var succElm = document.getElementById("mce-success-response-newsletter");
+
+        if (!inputValue || !inputValue.includes("@")) {
+            setDisplayAttribute(errElm, "d-block", "d-none");
+            setDisplayAttribute(succElm, "d-none", "d-block");
+
+            return;
+        }
+
+        analytics.identify(
+            inputValue,
+            { email: inputValue, storj_newsletter: true },
+            {},
+            function() {
+                analytics.track("storj_newsletter", {});
+
+                setDisplayAttribute(errElm, "d-none", "d-block");
+                setDisplayAttribute(succElm, "d-block", "d-none");
+            },
+        );
+
+        function setDisplayAttribute(element, classAdding, classRemoving) {
+            if (element) {
+                element.classList.add(classAdding);
+                element.classList.remove(classRemoving);
+            }
+        }
     }
 }
